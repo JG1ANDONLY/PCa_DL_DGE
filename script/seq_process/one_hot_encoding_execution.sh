ac6fd54dd73fb65ce8b94cf4d1557b020f145829
JG1ANDONLY <guozy@stanford.edu>
2024-10-30 05:53:46 +0000

    Used gzip to compress pkl data.

--- script/seq_process/one_hot_encoding_execution.sh
+++ script/seq_process/one_hot_encoding_execution.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 source ~/.bashrc
-jupyter nbconvert --to notebook --execute one_hot_encoding.ipynb --inplace > ../../log/one_hot_encoding_execution_log.txt 2>&1
+jupyter nbconvert --to notebook --execute one_hot_encoding.ipynb --inplace --ExecutePreprocessor.kernel_name=python3 > ../../log/one_hot_encoding_execution_log.txt 2>&1
+gzip ../../result/one_hot_encoding/gene_id_label_ohe.pkl
 echo "done"
